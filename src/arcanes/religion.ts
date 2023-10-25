0de2d174a3a0a5e1895289dac42d15eaa8717441
Iokanaan Goutier <iokanaan.goutier@gmail.com>
2023-10-25 20:15:51 +0200

    arcanes

--- src/arcanes/religion.ts
+++ src/arcanes/religion.ts
@@ -1,4 +1,4 @@
-import { effect } from "../utils/utils"
+import { effect, resetModifiers, setVirtualBg, signal } from "../utils/utils"
 
 export const setupArcanes = function(sheet: PavillonSheet) {
     effect(function() {
@@ -26,4 +26,44 @@ export const setupArcanes = function(sheet: PavillonSheet) {
     effect(function() {
         sheet.find("arcanes_faveurs").value(sheet.faveurs())
     }, [sheet.faveurs])
+
+    setupComp(sheet, "religion_arcane", "ERU")
+    setupComp(sheet, "obj_pouvoir", "CHA")
+
+
+    
+}
+
+const setupComp = function(sheet: PavillonSheet, compId: string, attr: AttributEnum) {
+
+    sheet.find(compId + "_label").on("click", function() {
+        const eff = sheet.find(compId + "_val").value() as number;
+        if(eff > 0) {
+            new RollBuilder(sheet.raw())
+                .expression(sheet.find(compId + "_val").value() + "d10 <={1:2} " + sheet.find(attr + "_val").value())
+                .title("")
+                .roll()
+        } else {
+            new RollBuilder(sheet.raw())
+                .expression("1d12 <={1:2} " + sheet.find(attr + "_val").value())
+                .title("")
+                .roll()
+        }
+        sheet.find(compId + "_val").virtualValue(null)
+        setVirtualBg(sheet.find(compId + "_val") as Component<number>)
+        resetModifiers(sheet)
+    })
+
+    sheet.find(compId + "_plus").on("click", function() {
+        const compCmp = sheet.find(compId + "_val") as Component<number>
+        compCmp.virtualValue(compCmp.value() + 1)
+        setVirtualBg(compCmp)
+    })
+    sheet.find(compId + "_minus").on("click", function() {
+        const compCmp = sheet.find(compId + "_val") as Component<number>
+        if(compCmp.value() > 0) {
+            compCmp.virtualValue(compCmp.value() - 1)
+            setVirtualBg(compCmp)
+        }
+    })
 }
